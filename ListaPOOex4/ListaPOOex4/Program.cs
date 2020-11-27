f26d7d4ee74bfe062f7163d46567549297a713bf
WaldirDMS <75144213+WaldirDMS@users.noreply.github.com>
2020-11-27 19:01:18 -0300

    Add files via upload

---
+++ ListaPOOex4/ListaPOOex4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ListaPOOex4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulo triangulo;
+
+            triangulo = new Triangulo();
+
+            Console.Write("Informe a base do triangulo: ");
+            triangulo.setBase(int.Parse(Console.ReadLine()));
+
+            Console.Write("Informe a altura do triangulo: ");
+            triangulo.setAltura(int.Parse(Console.ReadLine()));
+
+            triangulo.calcularArea();
+
+            Console.WriteLine("A area do triangulo é {0} ", triangulo.getArea());
+        }
+    }
+
+}
---
+++ ListaPOOex4/ListaPOOex4/Triangulo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ListaPOOex4
+{
+    class Triangulo
+    {
+        private int b;
+        private int h;
+        private int a;
+
+
+        public void setBase(int p)
+        {
+            b = p;
+        }
+
+        public void setAltura(int p)
+        {
+            h = p;
+        }
+
+        public int getBase()
+        {
+            return b;
+        }
+
+        public int getAltura()
+        {
+            return h;
+        }
+
+        public int getArea()
+        {
+            return a;
+        }
+
+        public void calcularArea()
+        {
+            a = (b * h)/2;
+        }
+    }
+}
